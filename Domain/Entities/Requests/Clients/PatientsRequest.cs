21dc0e759c7b4787845381f28783fc8c13035eab
Brian Nehemia <brian@algostudio.net>
2023-06-29 06:37:40 +0700

    Upadte model PatientsRequest

--- Domain/Entities/Requests/Clients/PatientsRequest.cs
+++ Domain/Entities/Requests/Clients/PatientsRequest.cs
@@ -2,11 +2,12 @@
 {
     public class PatientsRequest
     {
+        public int? OwnersId { get; set; }
         public string? Name { get; set; }
         public string? Species { get; set; }
         public string? Breed { get; set; }
         public string? Gender { get; set; }
         public DateTime? DateOfBirth { get; set; }
-        public bool? Vaccinated { get; set; }
+        public bool Vaccinated { get; set; }
     }
 }
