37e053afce06744b6f16b92ddeb0918032098a50
Brian Nehemia <brian@algostudio.net>
2023-07-13 02:47:28 +0700

    Tambah name user di model LoginResponse

--- Domain/Entities/Responses/Masters/LoginResponse.cs
+++ Domain/Entities/Responses/Masters/LoginResponse.cs
@@ -3,6 +3,7 @@
     public class LoginResponse
     {
         public int Id { get; set; }
+        public string Name { get; set; }
         public string Email { get; set; }
         public string Image { get; set; }
         public string Roles { get; set; }
