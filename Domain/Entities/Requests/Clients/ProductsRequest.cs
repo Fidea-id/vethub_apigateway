b3bfed465db801f10463b3db223b4bdc4ac168f9
Brian Nehemia <brian@algostudio.net>
2023-07-23 13:02:19 +0700

    Tambah ProductResponse, update ProductRequest

--- Domain/Entities/Requests/Clients/ProductsRequest.cs
+++ Domain/Entities/Requests/Clients/ProductsRequest.cs
@@ -6,7 +6,7 @@
         public string? Description { get; set; }
         public int? CategoryId { get; set; }
         public string? Price { get; set; }
-        public bool? IsBundle { get; set; } // Indicates if the product is a bundle
-        public bool? HasDiscount { get; set; } // Indicates if the product has discount or no
+        public bool IsBundle { get; set; } // Indicates if the product is a bundle
+        public bool HasDiscount { get; set; } // Indicates if the product has discount or no
     }
 }
---
+++ Domain/Entities/Responses/Clients/ProductResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Responses.Clients
+{
+    public class ProductResponse : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public double Price { get; set; }
+        public bool IsBundle { get; set; }
+        public bool HasDiscount { get; set; }
+    }
+}
