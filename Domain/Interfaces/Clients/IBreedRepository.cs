07c44fa61efa98db4232d4bae978bae8799abd51
Andhika Kurnia <kurniaandhika18@gmail.com>
2023-09-14 01:39:23 +0700

    - fix issue duplicate breed - add check animal and breed first at create

--- Domain/Interfaces/Clients/IBreedRepository.cs
+++ Domain/Interfaces/Clients/IBreedRepository.cs
@@ -7,7 +7,7 @@ namespace Domain.Interfaces.Clients
 {
     public interface IBreedRepository : IGenericRepository<Breeds, NameBaseEntityFilter>
     {
-        Task<Breeds> GetByName(string dbName, string name);
+        Task<Breeds> GetByName(string dbName, int speciesId, string name);
         Task<BreedAnimalResponse> GetBreedAnimal(int id, string dbName);
         Task<IEnumerable<BreedAnimalResponse>> GetBreedAnimalListByAnimal(int idAnimal, string dbName);
         Task<DataResultDTO<BreedAnimalResponse>> GetBreedAnimalList(NameBaseEntityFilter filter, string dbName);
