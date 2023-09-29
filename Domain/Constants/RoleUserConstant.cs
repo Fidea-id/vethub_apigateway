7224f64796a951e6159e172d3541a167dbad5b11
Andhika Kurnia <kurniaandhika18@gmail.com>
2023-09-30 05:12:47 +0700

    - role layout

---
+++ Domain/Constants/RoleUserConstant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Constants
+{
+    public class RoleUserConstant
+    {
+        public RoleUserConstant()
+        {
+        }
+        public const string SUPERADMIN = "Superadmin";
+        public const string OWNERS = "Owners";
+        public const string ADMIN = "Admin";
+        public const string DOCTOR = "Doctor";
+    }
+}
