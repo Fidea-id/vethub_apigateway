53ece632e78e2931bbae7f8b48e5a6815b180d97
rafkandos <rafflitok@gmail.com>
2023-10-04 14:37:21 +0700

    update medical records

---
+++ Domain/Entities/Responses/Clients/PrescriptionItemResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Entities.Responses.Clients
+{
+    public class PrescriptionItemResponse : BaseEntity
+    {
+        public int Id { get;set; }
+        public string Name { get;set; }
+        public string Description { get;set; }
+        public int stock { get;set; }
+        public string Volume { get; set; }
+        public int Price { get; set; }
+        public string Type { get; set; }
+    }
+}
