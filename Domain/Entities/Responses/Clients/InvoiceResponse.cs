037fc7afd2f20a04fc40c2436e9be2548a4f506d
Andhika Kurnia <kurniaandhika18@gmail.com>
2024-02-24 16:33:04 +0700

    update model invoice response

--- Domain/Entities/Responses/Clients/InvoiceResponse.cs
+++ Domain/Entities/Responses/Clients/InvoiceResponse.cs
@@ -11,8 +11,10 @@ namespace Domain.Entities.Responses.Clients
     public class InvoiceResponse
     {
         public MedicalRecordsDetailResponse Detail { get; set; }
+        public OrderFullResponse DetailOrder { get; set; }
         public Clinics ClinicData { get; set; }
         public IEnumerable<OrdersPayment> PaymentData { get; set; }
+        public IEnumerable<OrdersPaymentResponse> PaymentDataResponse { get; set; }
         public IEnumerable<PaymentMethod> PaymentMethodData { get; set; }
 }
 }
