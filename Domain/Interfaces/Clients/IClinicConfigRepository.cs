2cfe2d295ad945025615d3dd1d13318992180f76
Andhika Kurnia <kurniaandhika18@gmail.com>
2024-07-23 13:08:29 +0700

    -add devexpress

--- Domain/Interfaces/Clients/IClinicConfigRepository.cs
+++ Domain/Interfaces/Clients/IClinicConfigRepository.cs
@@ -6,6 +6,7 @@ namespace Domain.Interfaces.Clients
     public interface IClinicConfigRepository : IGenericRepository<ClinicConfig, NameBaseEntityFilter>
     {
         Task<int> AddConfig(string dbName, ClinicConfig config);
+        Task AddConfigRange(string dbName, IEnumerable<ClinicConfig> config);
         Task UpdateConfig(string dbName, ClinicConfig config);
         Task<string> GetConfigValue(string dbName, string key);
         Task<ClinicConfig> GetConfigByKey(string dbName, string key);
